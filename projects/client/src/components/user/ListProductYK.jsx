dd306cfb9fba5bd82d13506ea1144a2e37e702e2
M Fahrur Rizky <70459040+fahrurrizky@users.noreply.github.com>
2023-10-14 22:01:58 +0700

    Ega 29 28 (#19)
    * user address
    
    * list product & product detail
    
    * product list
    
    * Pull done
    
    * cart hilang

--- projects/client/src/components/user/ListProductYK.jsx
+++ projects/client/src/components/user/ListProductYK.jsx
@@ -32,13 +32,6 @@ export default function Product() {
   const [category, setCategory] = useState("");
   const [name, setName] = useState("");
   const [totalPages, setTotalPages] = useState(0);
-  const [totalPages, setTotalPages] = useState("");
-
-  useEffect(() => {
-    // Initialize quantities state with default quantity (1) for each product
-    const initialQuantities = new Array(product.length).fill(1);
-    setQuantities(initialQuantities);
-  }, [product]);
 
   const handleSearch = (query) => {
     setSearchQuery(query);
--- projects/client/src/components/user/ProfileIcon.jsx
+++ projects/client/src/components/user/ProfileIcon.jsx
@@ -14,6 +14,7 @@ import {
 } from "@chakra-ui/react";
 import { BsFillCartCheckFill } from "react-icons/bs";
 import React, { useState, useEffect } from "react";
+import CartModal from "./Cart"
 import axios from "axios";
 
 
@@ -22,6 +23,13 @@ const Profile = () => {
   const [userData, setUserData] = useState({ name: '', avatar: '' });
   const [isCartModalOpen, setIsCartModalOpen] = useState(false);
 
+  const openCartModal = () => {
+    setIsCartModalOpen(true);
+  };
+  
+  const closeCartModal = () => {
+    setIsCartModalOpen(false);
+  };
   
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -55,6 +63,14 @@ const Profile = () => {
       >
         <Flex ml={"5"}>
           <Flex>
+            {/* Cart */}
+            <Button variant="ghost" mr={3} _hover={'black'} >
+              <BsFillCartCheckFill color="white" size={25} onClick={openCartModal} />
+              <CartModal
+                isOpen={isCartModalOpen}
+                onClose={closeCartModal}
+              />
+            </Button>
             {/* Menu Profile */}
             <Menu>
               <MenuButton as={Button} size="sm" p={0} rounded="full" mt={"1"}>
